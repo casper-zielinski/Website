e1613290afdf36bb133d22746fa50dbdce8967a6
Zielinski Casper <casper.zielinski.endercasper@gmail.com>
2025-05-19 14:18:23 +0200

    Time Converter

--- script/timeconverter.js
+++ script/timeconverter.js
@@ -6,4 +6,11 @@ dropdownButton.addEventListener("click", function() {
   dropdownContent.style.display = (dropdownContent.style.display === "block") ? "none" : "block";
 });
 
+const dropdownButton2 = document.getElementById("dropdownButton2");
+const dropdownContent2 = document.getElementById("TimeDropdown2");
+
+dropdownButton2.addEventListener("click", function() {
+  dropdownContent2.style.display = (dropdownContent2.style.display === "block") ? "none" : "block";
+});
+
 
