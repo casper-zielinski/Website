73540ee8f4b82e5b9c40ad0e842dd37170fc1d56
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2025-05-21 11:13:11 +0200

    Added Switch logik to dataconverter

--- script/dataconverter.js
+++ script/dataconverter.js
@@ -11,21 +11,39 @@ const convertButton = document.getElementById("convert");
 const fromSelect = document.querySelector("#calculate-from");
 const toSelect = document.querySelector("#calculate-to");
 const resultField = document.querySelector("#convert-result");
+const switchButton = document.querySelector('.switch-arrows');
 
+// Standardwerte setzen beim Laden
+window.addEventListener('load', () => {
+    fromSelect.value = 'Bit';
+    toSelect.value = 'Byte';
+});
 
-convertButton.addEventListener("click", function () {
+// Umrechnungsfunktion
+function convertUnits() {
     const inputValue = parseFloat(userInput.value);
     const fromUnit = fromSelect.value;
     const toUnit = toSelect.value;
 
     if (isNaN(inputValue)) {
-        resultField.value = "Input is not a valid number!";
+        resultField.value = '';
         return;
     }
 
     const valueInBytes = inputValue * units[fromUnit];
-
     const result = valueInBytes / units[toUnit];
 
     resultField.value = result.toFixed(12);
+}
+
+// Umrechnung bei Button-Klick
+convertButton.addEventListener("click", convertUnits);
+
+// Switch-Button zum Tauschen der Einheiten + automatische Umrechnung
+switchButton.addEventListener('click', () => {
+    const temp = fromSelect.value;
+    fromSelect.value = toSelect.value;
+    toSelect.value = temp;
+
+    convertUnits();
 });
